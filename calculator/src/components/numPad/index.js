2310d8c4200eae62b0060db1c764426b9c3582db
veritas <rose.daemon.belmont@gmail.com>
2021-08-18 18:08:11 -0700

    creates num pad component

---
+++ calculator/src/components/numPad/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NumButton from './NumberButt';
+
+const NumPad = (props) => {
+    return (
+        <div>
+            <NumButton />
+        </div>
+    );
+};
+
+export default NumPad;
